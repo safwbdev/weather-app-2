320c032a3ac3c77f19ccd209b3e35e7da70aa318
Stefaan <safwebdev@gmail.com>
2020-12-14 22:44:35 +0800

    local storage code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    user: "",
+    rememberMe: false,
+  };
+
+  componentDidMount() {
+    const rememberMe = localStorage.getItem("rememberMe") === "true";
+    const user = rememberMe ? localStorage.getItem("user") : "";
+    this.setState({ user, rememberMe });
+  }
+
+  handleChange = (event) => {
+    const input = event.target;
+    const value = input.type === "checkbox" ? input.checked : input.value;
+
+    this.setState({ [input.name]: value });
+  };
+
+  handleFormSubmit = () => {
+    const { user, rememberMe } = this.state;
+    localStorage.setItem("rememberMe", rememberMe);
+    localStorage.setItem("user", rememberMe ? user : "");
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <label>
+          User:{" "}
+          <input
+            name="user"
+            value={this.state.user}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          <input
+            name="rememberMe"
+            checked={this.state.rememberMe}
+            onChange={this.handleChange}
+            type="checkbox"
+          />{" "}
+          Remember me
+        </label>
+        <button type="submit">Sign In</button>
+      </form>
+    );
+  }
 }
 
 export default App;
