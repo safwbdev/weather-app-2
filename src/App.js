a6fd5d300286da28ea824722382e94cb69e02088
Stefaan <safwebdev@gmail.com>
2020-12-19 18:58:17 +0800

    city selection

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import cityList from "./city.list.min.json";
 import { TextField } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
-// import ComboBox from "./test";
 
 class App extends Component {
-  state = {
-    user: "",
-    rememberMe: false,
-    towns: [],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: "",
+      rememberMe: false,
+      towns: [],
+      selectedTown: null,
+    };
+    this.onTagsChange = this.onTagsChange.bind(this);
+  }
 
   componentDidMount() {
     cityList.map(({ name, country }) => {
@@ -22,15 +26,29 @@ class App extends Component {
     });
   }
 
+  onTagsChange = (event, values) => {
+    this.setState(
+      {
+        selectedTown: values,
+      },
+      () => {
+        // This will output an array of objects
+        // given by Autocompelte options property.
+        console.log(this.state.selectedTown);
+      }
+    );
+  };
+
   render() {
-    const { towns } = this.state;
+    const { towns, selectedTown } = this.state;
     return (
       <div>
-        <h1>test</h1>
+        <h1>{selectedTown}</h1>
         <Autocomplete
           id="combo-box-demo"
           options={towns}
           getOptionLabel={(option) => option}
+          onChange={this.onTagsChange}
           style={{ width: 300 }}
           renderInput={(params) => (
             <TextField {...params} label="Combo box" variant="outlined" />
